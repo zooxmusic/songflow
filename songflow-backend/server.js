9c4b3274f0932a5c7513ebe737bdbf4fb66c755a
Brian Szucs <brian@zooxmusic.com>
2025-02-01 18:03:58 -0500

    trying 3000 again

--- songflow-backend/server.js
+++ songflow-backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 80;  // Or 3000 if you're using a custom port
+const PORT = 3000;  // Or 3000 if you're using a custom port
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
